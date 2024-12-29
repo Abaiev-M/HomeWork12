854743b1f6492883b887fbfe1b323a2ed1a110d2
Abaiev-M <abaievmisha@gmail.com>
2024-12-29 15:36:11 +0200

    The projects were created

---
+++ SwiftUI/Lesson12SwiftUI/Lesson12SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lesson12SwiftUI
+//
+//  Created by Misha Abaiev on 29.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/Lesson12SwiftUI/Lesson12SwiftUI/Lesson12SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson12SwiftUIApp.swift
+//  Lesson12SwiftUI
+//
+//  Created by Misha Abaiev on 29.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Lesson12SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
