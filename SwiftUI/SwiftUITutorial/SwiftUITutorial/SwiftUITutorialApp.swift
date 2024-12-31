af8bd5d532a44322ca7a806756816c82227c76f0
Abaiev-M <abaievmisha@gmail.com>
2024-12-31 16:26:36 +0200

    Done2

---
+++ SwiftUI/SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Misha Abaiev on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CollectionView()
+        }
+    }
+}
