567f92f9e2a59c2b26dca5dbe841d68733b6eff4
Abaiev-M <abaievmisha@gmail.com>
2024-12-31 16:20:27 +0200

    Done

---
+++ UIKit/Lesson12UIKit/Lesson12UIKit/CustomCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionViewCell.swift
+//  Lesson12UIKit
+//
+//  Created by Misha Abaiev on 29.12.2024.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
